6c9854bdfd9e70675604f206ff1eb7733c7a7a4d
ninodimitriadi <nino.dimitriadi.1@btu.edu.ge>
2020-10-29 12:03:44 +0400

    first commit

---
+++ src/main 1.kt
@@ -0,0 +1,751 @@
+import java.awt.Color
+import kotlin.io.println as println1
+
+//import IFigure as
+
+//fun main(){
+//    val age = 24
+//    when (age){
+//      0-> print("check parametre")
+//
+//        in 1..16 ->{
+//            print ("you cant watvch tis movie")
+//
+//        }
+//        17,18 -> print ("you can watch tis movie with parents")
+//
+//        in 19 ..21 -> print("film bilonegs horore genre")
+//
+//        else -> print ("you cam watch this movie")
+//
+//
+//    }
+
+
+
+
+//fun main (){
+//    for (i in 10 downTo 1 step 3){
+//        println (i)
+//    }
+//
+//
+//}
+
+//fun main (){
+//    var x = 5
+//
+//    while(x < 10){
+//    println (x)
+//    x++}
+//
+//}
+
+
+//fun happyBirthday (age: Int, name: String ){
+//    return
+//}
+//fun main(){
+//        val x = 3
+//when (x) {
+//        1 -> print("first")
+//        2, 3 -> print("second")
+//        in 4..10 -> print("third")
+//        else -> print ("else")
+//val age = 12
+//
+//when (age)  {
+//        0 -> print ("check parametre")
+//        in 1..16 -> {
+//                print ("yoc can't watch this movie")
+//        }
+//        17,18 -> print ("you can watch this movie with your parents")
+//        in 19..21 ->{
+//                print ("this film belongs horor ganres")
+//        }
+//        else -> print ("you can watch this movie")
+//fun main(){
+//    for (i in 1..10) {
+//            println(i*4)
+//    }
+//
+//
+//    }
+
+//fun main() {
+//        for (i  in 1 until 7){
+//                println (i)
+//        }
+//
+//
+//
+//
+//}
+
+//fun main () {
+//        for (i in 2 until 5)
+//                println(i)
+//
+//
+//}
+
+//fun main (){
+//        for (i in 10 downTo 1 )
+//                println (i)
+//
+//
+//
+//}
+
+//fun main (){
+//        for (i in 10 downTo  1 step 2)
+//                println (i)
+//
+//
+//
+//}
+
+//
+//fun main (){
+//        for (i in 1 until 20 step 3)
+//                println (i)
+//
+//
+//
+//}
+
+
+//fun main (){
+//
+//        for (i in 10 downTo 1 step 2 )
+//                when (i) {
+//                  2,3  -> {
+//                        println(i)
+//                }
+//
+//        }
+//}
+
+
+//fun main (){
+//        var sum  = 0
+//        for (i in 1 downTo -10 step 2){
+//              println (i)
+//        }
+//
+
+
+//fun main (){
+//        val age = 85
+//        when (age) {
+//           in 0..5 -> print("you must go to the kindergarten")
+//           in 6..18 -> print("you must go to the school")
+//           in 18..21 -> print("you should study in university")
+//           in 21..30 -> print ("you must work")
+//           31,32 -> print("you should married")
+//           else -> print ("you must work and be independent")
+//
+//
+//        }
+
+//fun main (){
+//        val salary = 17589
+//        when (salary){
+//                in 20..100 -> print ("get up and shake your lazy ass")
+//                in 100..150 -> print ("you must work hard if you want more")
+//                in 151..300 -> print ("better but that not enought")
+//                in 301..500 -> print ("come on you diserve more ")
+//                in 501..1000 -> print ("you are not happy you want much more")
+//                in 1001..2000 -> print("if you can output this much than you can output more")
+//                in 2001..5000 -> print("well done i always belive in you")
+//                else -> print ("dont be pig borrow me some money")
+//
+//
+//
+//        }
+
+//fun main (){
+//        for (i in 1..10)
+//                print(i)
+//        for (i in 1 until 5)
+//                print (i)
+//        for (i in 10 downTo  1 )
+//                print(i)
+//        for (i in 5 downTo 0 step 2)
+//                print(i)
+//}
+
+
+//fun main () {
+//        var x = 0
+//        while (x <= 10) {
+//                println(x)
+//                x += 5 }
+//}
+
+//fun main (){
+//        var age = 18
+//        while (age >= 18){
+//                print("you are minor")
+//
+//        }
+//
+//
+//
+//
+//}
+
+//fun main() {
+//        var age = 0
+//        while (age < 18){
+//                println ("yoy are minor ")
+//                age++
+//
+//
+//        }
+//
+//
+//
+//
+//}
+//
+
+
+//fun happyBirthday (age: Int, name: String){
+//        return
+//
+//
+//
+//
+
+
+//fun main () {
+//    val age = 28
+//    when  (age) {
+//        in 0..18 -> print("you are minor ")
+//        else -> print ("you are of age")
+//
+//    }
+//
+//
+//}
+
+
+//fun main (){
+//    for (i in 1..10) {
+//        println(i*5)
+//    }
+//
+//}
+
+//fun main (){
+//   for (i in 1 until 10)
+//       println(i)
+//
+//
+//
+//}
+
+
+//fun main (){
+//    for (i in 10 downTo 0)
+//        println(i)
+//}
+
+//fun main (){
+//    for (i in 1..10)
+//        println("hello")
+//
+//}
+
+
+//fun main (){
+//    for (i in 1 until 20 step 2)
+//        println(i)
+//
+//
+//
+//}
+
+//fun main(){
+//    val  result = happyBirthday(23,"vaja")
+//    println(result)
+//    val resultt = happyBirthday(12,"salo")
+//    print(resultt)
+//}
+//
+//
+//
+//
+//fun happyBirthday(age: Int,name: String): String {
+//    return "Happy ${age}th Birthday, $name."
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fun main (){
+//    var sum = 0
+//    for (i in 1..4)
+//        sum += i
+//    print (sum)
+//
+//
+//}
+
+
+//fun main () {
+//    var sum = 0
+//    for (i in 2 until 10 step 2)
+//        sum += i
+//    print(sum)
+//
+//}
+
+//fun main (){
+//    var sum = 0
+//    for (i in 8 downTo 3 step 2)
+//        sum += i
+//    print(sum)
+//
+//}
+
+//fun main (){
+//    var x = 5
+//    while (x < 10){
+//        x ++
+//    println ("hi")
+//    }
+//
+//}
+
+//fun main (){
+//    var x = 23
+//    while (x > 21){
+//        x -= 1
+//        println("stop")
+//
+//    }
+//
+//
+//}
+
+
+//
+
+//
+//fun main(){
+//    val rav4: carFactory = carFactory()
+//    rav4.name = "Totota Rav 4"
+//    rav4.color = "yellow"
+//    rav4.withTurbo = false
+//    rav4.maxSpeed = 180
+//    rav4.printCarInfo()
+//    rav4.coloring( "Yellow")
+//    rav4.coloring( "Red")
+//    rav4.printCarInfo()
+//
+//    val tucson = carFactory()
+//    tucson.name = "Hyundai Tucson"
+//    tucson.year = 2012
+//    tucson.color = "blue"
+//    tucson.maxSpeed = 200
+//    tucson.withTurbo = true
+//    tucson.printCarInfo()
+//
+//    var rav4_1 = rav4
+//    rav4_1.coloring("black")
+//    rav4.printCarInfo()
+//
+//
+//
+//}
+//class carFactory {
+//    var name: String = ""
+//    var maxSpeed: Int = 0
+//    var year: Int = 0
+//    var withTurbo: Boolean = false
+//    var color: String = ""
+//
+//    fun printCarInfo(){
+//        println("$name - $year - $color")
+//    }
+//    fun coloring (color: String){
+//        if (color == this.color){
+//            println("same color")
+//        }else{
+//            println ("coloring...")
+//            this.color = color
+//
+//        }
+//
+//    }
+//
+//}
+
+//fun main(){
+//
+//    var f1 = Fraction(3F,4F)
+//
+//
+//    var f2 = Fraction(6F,8F)
+//
+//    println (f1 == f2)
+//
+//    println (f1.equals(f2))
+//
+//
+//
+//}
+//class Fraction (n:Float, d: Float){
+//
+//    private var numerator: Float = n
+//    private var denominator: Float = d
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (other is Fraction) {
+//            if (numerator * other.denominator / denominator == other.numerator)
+//                return true
+//        }
+//
+//        return false
+//    }
+//}
+//
+
+
+//fun main(){
+//
+//
+//}
+//
+//interface IFigure{
+//    fun getArea (): Float
+//    fun getPerimeter (): Float
+//
+//}
+//
+//class Rectangle(width: Float,height: Float){
+//
+//    override fun gerArea
+//
+//}
+
+
+//fun main(){
+//
+//    val rav4: CarFactory = CarFactory()
+//    rav4.name = "toyota rav4"
+//    rav4.color = "yellow"
+//    rav4.withTurbo = false
+//    rav4.year = 2017
+//    rav4.maxSpeed = 180
+//
+//    rav4.printCarInfo()
+//    rav4.coloring("yellow")
+//    rav4.coloring("red")
+//    rav4.printCarInfo()
+//
+//    var tucson:CarFactory? = CarFactory ()
+//    tucson?.name = "Hyndai Tucson"
+//    tucson?.year = 2012
+//    tucson?.color = "Blue"
+//    tucson?.maxSpeed = 200
+//    tucson?.withTurbo = true
+//
+//
+//
+//
+//    var rav4_1 = rav4
+//    rav4_1.coloring("black")
+//    rav4.printCarInfo()
+//
+//    tucson = null
+//    tucson?.printCarInfo()
+//
+//
+//}
+//class CarFactory{
+//    var name: String = ""
+//    var maxSpeed: Int = 0
+//    var year: Int = 0
+//    var withTurbo: Boolean = false
+//    var color: String = ""
+//
+//    fun printCarInfo(){
+//        println("$name - $year - $color ")
+//    }
+//    fun coloring (color: String) {
+//        if (color == this.color){
+//            println ("same color")
+//            }else {
+//            println("Coloring...")
+//            this.color = color
+//        }
+//    }
+//}
+
+//fun main (){
+//    val cocacola = Drinkfactory()
+//    cocacola.name = "coca-cola classic"
+//    cocacola.size = 2
+//    cocacola.color = "brown"
+//    cocacola.count = 4
+//
+//    cocacola.printfactoryInfo()
+//    cocacola.coloring( "black")
+//    cocacola.printfactoryInfo()
+//
+//    val fanta = Drinkfactory()
+//    fanta.name = "fanta tropic"
+//    fanta.size = 1
+//    fanta.color = "yellow"
+//    fanta.count = 1
+//
+//    fanta.printfactoryInfo()
+//    fanta.coloring("red")
+//    fanta.printfactoryInfo()
+//
+//
+//
+//}
+//
+//class Drinkfactory{
+//    var name = ""
+//    var size = 0
+//    var color = ""
+//    var count = 0
+//
+//    fun printfactoryInfo(){
+//        println("$name - $size - $color - $count")
+//
+//    }
+//    fun coloring (color: String){
+//        if (this.color == color)
+//            println("same color")
+//        else{
+//            println("colloring...")
+//            this.color = color
+//        }
+//    }
+//
+//}
+
+//fun main(){
+//    var f1 = Fraction(3F,4F)
+//
+//
+//    var f2 = Fraction(6F, 8F)
+//
+//    println(f1 == f2)
+//
+////    println(f1)
+////    println(f2)
+//
+//
+//
+//
+//}
+//class Fraction (n:Float, d:Float){
+//
+//    private var numerator: Float = n
+//    private var denominator: Float = d
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (other is Fraction) {
+//            if (numerator * other.denominator / denominator == other.numerator) {
+//                return true
+//            }
+//
+//
+//        }
+//        return false
+//
+//    }
+//
+//    override fun toString(): String {
+//        return "$numerator/$denominator"
+//
+//    }
+//
+//}
+
+//fun main(){
+//    val r = Rectangle(3F, 4F)
+//    println(r.getArea())
+//    println(r)
+//
+//
+//}
+//interface IFigure{
+//    fun getArea():Float
+//    fun getPerimeter():Float
+//}
+//
+//class Rectangle(private val widht:Float,private val height:Float ):IFigure{
+//    override fun getArea(): Float {
+//        return widht * height
+//    }
+//
+//    override fun getPerimeter(): Float {
+//        return 2 * (widht + height)
+//    }
+//
+//    override fun toString(): String {
+//        return "$widht; $height"
+//    }
+//
+//}
+
+//fun main(){
+//    var x:Float = 4F
+//    var y:Float = 4F
+//
+//    println(x == y)
+//    println(x)
+//    println(y)
+//
+//
+//}
+//interface Imove {
+//    fun getMove(): Float
+//
+//}
+//
+//
+//
+//
+//class Point() {
+//    var x: Float = 4F
+//    var y: Float = 4F
+//
+//    override fun toString(): String {
+//        return "($x, $y)"
+//    }
+//
+//    override fun equals(other: Any?):Boolean {
+//        if (other is Point) {
+//            if (x == y) {
+//                println("they are equals")
+//            } else {
+//                println("they are not equals")
+//
+//            }
+//        }
+//
+//    fun getMove (): Float {
+//        return (-1 * x)
+//
+//
+//    }
+//
+//
+//    }
+//
+//
+//
+//}
+
+
+//fun main (){
+//    val r = Point(3F,3F)
+//
+//    println(r.getMove())
+//    println(r.toString())
+//    println(r.equals())
+
+//}
+//
+//interface IMove{
+//     val True: Float
+//
+//    fun getMove():Float
+//
+//}
+//
+//class Point(private val x:Float, private val y:Float):IMove {
+//    override fun getMove(): Float {
+//        return -1 * x
+//    }
+//
+//    override fun toString(): String {
+//        return "$x, $y"
+//    }
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (other is Point) {
+//            if ((x == other.x) and (y == other.y){
+//                return true
+//            }
+//        } else {
+//            return false
+//        }
+//
+//    }
+//}
+
+//დავალება!!!!!!!!!!!!!!
+//fun main (){
+//    var a1 = Point(2F,3F)
+//    var a2 = Point(2F,3F)
+//
+//
+//    kotlin.io.println(a1 == a2)
+//    kotlin.io.println(a1.toString())
+//    kotlin.io.println(a1.axis())
+//
+//
+//}
+//
+//class Point(x:Float, y:Float ){
+//
+//
+//
+//    var x:Float = x
+//    var y:Float = y
+//
+//
+//    override fun equals(other: Any?): Boolean {
+//        if (other is Point){
+//            if ((x == other.x) and (y == other.y ))
+//                return true
+//        }
+//        return false
+//
+//        }
+//
+//    override fun toString(): String {
+//        return "($x; $y)"
+//    }
+//
+//    fun axis(){
+//        x = -x
+//        return println1("სიმეტრია Y ღერძის მიმართ იქნება ($x,$y)")
+//    }
+//
+//}
+
+
+
+
